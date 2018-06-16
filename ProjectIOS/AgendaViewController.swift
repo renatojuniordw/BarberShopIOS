e3b70c9f415f70fa710af26841ad392216eda1d4
Treinamento <treinamento@iMac-de-Suporte-6.local>
2018-06-16 18:05:19 -0300

    commit 4 finsh

--- ProjectIOS/AgendaViewController.swift
+++ ProjectIOS/AgendaViewController.swift
@@ -1,49 +1,56 @@
-//
-//  AgendaViewController.swift
-//  ProjectIOS
-//
-//  Created by Treinamento on 19/05/2018.
-//  Copyright © 2018 Treinamento. All rights reserved.
-//
-
 import UIKit
 import Firebase
 import FirebaseAuth
 
-class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AgendaViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var ref: DatabaseReference!
     var arr: [String] = []
     
+    @IBOutlet weak var TableVIew: UITableView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
         ref = Database.database().reference()
         ref.child("Agendamentos").observeSingleEvent(of: .value, with: { (snapshot) in
-            //print(snapshot)
+           
             let val = snapshot.value as! NSDictionary
             for element in val {
-                
-                print("Usuario aqui \n\n\n\n\n\n")
+
                 let data = element.value as! [String:Any]
 
                 if let day = data["day"] {
                     if let month = data["month"] {
                         if let year = data["year"] {
-                            var aux = "\(day) - \(month) - \(year)"
+                            var aux = "\(day)/\(month)/\(year)"
                             self.arr.append(aux)
                         }
                     }
                 }
-                
-                print(self.arr)
+
             }
+            
+            self.TableVIew.delegate = self
+            self.TableVIew.dataSource = self
             
             }) { (error) in
             print(error.localizedDescription)
         }
         
-
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TableVIew.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = self.arr[indexPath.row]
+        //cell?.detailTextLabel?.text = "\(arr[indexPath.row])"
         
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
